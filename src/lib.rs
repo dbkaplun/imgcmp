e56b69b7bb6361de08f369da2365a99ebe3e42ac
Matt Brubeck <mbrubeck@limpet.net>
2018-06-20 17:55:45 -0700

    Create a Splitter trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 extern crate image;
 extern crate num_traits;
 
-use image::{GenericImageView, Pixel};
+use image::{GenericImageView, Pixel, SubImage};
 use num_traits::ToPrimitive;
 
-mod image_view;
 mod util;
 
-use image_view::ImageView;
-use util::{color_dist2, newtons_method};
+use util::newtons_method;
 
 #[derive(Clone, Copy)]
 enum SplitDirection {
@@ -24,7 +22,8 @@ struct Split {
 }
 
 impl Split {
-    fn split<'i, I: GenericImageView, V: ImageView<I>>(&self, image: &'i V) -> (&'i V, &'i V) {
+    fn split<'i, I: GenericImageView>(&self, image: &'i I) -> (SubImage<&'i I::InnerImageView>,
+                                                               SubImage<&'i I::InnerImageView>) {
         let (x, y, w, h) = image.bounds();
         match self.dir {
             SplitDirection::Horiz => (
@@ -51,26 +50,23 @@ struct NodeChild<P: Pixel> {
 }
 
 impl<P: Pixel> Node<P> {
-    fn split_default<I: GenericImageView<Pixel = P>, V: ImageView<I>>(image: &V) -> Self {
-        Self::new_from_image_and_split(image, &default_split)
+    fn split_default<I: GenericImageView<Pixel = P>>(image: &I) -> Self {
+        Self::new_from_image_and_split::<DefaultSplit, _>(image)
     }
 
-    fn new_from_image_and_split<F, I: GenericImageView<Pixel = P>, V: ImageView<I>>(
-        image: &V,
-        split_fn: &F,
+    fn new_from_image_and_split<F: Splitter, I: GenericImageView<Pixel = P>>(
+        image: &I,
     ) -> Self
-    where
-        F: Fn(&V) -> Split,
     {
-        let split = split_fn(&image);
+        let split = F::split(image);
         Self {
-            color: image.color_avg(),
+            color: unimplemented!(), // image.color_avg(),
             // child: None,
             child: if split.size > 1 {
                 let (a, b) = split.split(image);
                 Some(Box::new(NodeChild {
-                    a: Self::new_from_image_and_split(a, split_fn),
-                    b: Self::new_from_image_and_split(b, split_fn),
+                    a: Self::new_from_image_and_split::<F, _>(&a),
+                    b: Self::new_from_image_and_split::<F, _>(&b),
                     split,
                 }))
             } else {
@@ -80,59 +76,75 @@ impl<P: Pixel> Node<P> {
     }
 }
 
-fn default_split<I: GenericImageView, V: ImageView<I>>(image: &V) -> Split {
-    let (w, h) = image.dimensions();
-    if w > h {
-        Split {
-            dir: SplitDirection::Horiz,
-            at: w / 2,
-            size: w,
-        }
-    } else {
-        Split {
-            dir: SplitDirection::Horiz,
-            at: h / 2,
-            size: h,
+trait Splitter {
+    fn split<I: GenericImageView>(image: &I) -> Split
+    where
+        <I::Pixel as Pixel>::Subpixel: ToPrimitive;
+}
+
+struct DefaultSplit;
+impl Splitter for DefaultSplit {
+    fn split<I: GenericImageView>(image: &I) -> Split
+    where
+        <I::Pixel as Pixel>::Subpixel: ToPrimitive
+    {
+        let (w, h) = image.dimensions();
+        if w > h {
+            Split {
+                dir: SplitDirection::Horiz,
+                at: w / 2,
+                size: w,
+            }
+        } else {
+            Split {
+                dir: SplitDirection::Horiz,
+                at: h / 2,
+                size: h,
+            }
         }
     }
 }
 
-fn best_split<I: GenericImageView, V: ImageView<I>>(image: &V) -> Split
-where
-    <I::Pixel as Pixel>::Subpixel: ToPrimitive,
-{
-    let (w, h) = image.dimensions();
-    let (size, dir) = if w > h {
-        (w, SplitDirection::Horiz)
-    } else {
-        (h, SplitDirection::Vert)
-    };
-    let split_frac = newtons_method(
-        &|n| {
-            let (a, b) = match dir {
-                SplitDirection::Horiz => {
-                    let split = (n * w as f64) as u32;
-                    (
-                        &image.view(0, 0, split, h),
-                        &image.view(split, 0, w - split, h),
-                    )
-                }
-                SplitDirection::Vert => {
-                    let split = (n * h as f64) as u32;
-                    (
-                        &image.view(0, 0, w, split),
-                        &image.view(0, split, w, h - split),
-                    )
-                }
-            };
-            color_dist2(a.color_avg(), b.color_avg()).to_f64().unwrap()
-        },
-        0.5,
-    ).unwrap();
-    Split {
-        dir,
-        size,
-        at: (split_frac * size as f64) as u32,
+struct BestSplit;
+
+impl Splitter for BestSplit {
+    fn split<I: GenericImageView>(image: &I) -> Split
+    where
+        <I::Pixel as Pixel>::Subpixel: ToPrimitive
+    {
+        let (w, h) = image.dimensions();
+        let (size, dir) = if w > h {
+            (w, SplitDirection::Horiz)
+        } else {
+            (h, SplitDirection::Vert)
+        };
+        let split_frac = newtons_method(
+            &|n| {
+                let (a, b) = match dir {
+                    SplitDirection::Horiz => {
+                        let split = (n * w as f64) as u32;
+                        (
+                            image.view(0, 0, split, h),
+                            image.view(split, 0, w - split, h),
+                        )
+                    }
+                    SplitDirection::Vert => {
+                        let split = (n * h as f64) as u32;
+                        (
+                            image.view(0, 0, w, split),
+                            image.view(0, split, w, h - split),
+                        )
+                    }
+                };
+                unimplemented!() //color_dist2(a.color_avg(), b.color_avg()).to_f64().unwrap()
+            },
+            0.5,
+        ).unwrap();
+        Split {
+            dir,
+            size,
+            at: (split_frac * size as f64) as u32,
+        }
     }
 }
 
